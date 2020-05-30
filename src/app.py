b91c53c199d96b2147f358ebea5f87d841ee9182
Laura Levy <llevy510@gmail.com>
2020-05-30 13:14:40 -0400

    added a new handler for a new event

--- src/app.py
+++ src/app.py
@@ -4,5 +4,8 @@ dotenv.load_dotenv()
 import json
 from os import environ
 
-def handler(event, context): 
+def handler(event, context):
+    return json.dumps({"vars": environ.copy()})
+
+def second_handler(second_event, context):
     return json.dumps({"vars": environ.copy()})
